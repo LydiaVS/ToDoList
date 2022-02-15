dd521830ed032c91dafbf10c71ebf59a2316294e
Nerea05 <nereaortizcardo666@gmail.com>
2022-02-15 21:22:55 +0100

    “responsive1”

--- app/main.js
+++ app/main.js
@@ -5,13 +5,18 @@ const ul = document.querySelector("ul");
 add.addEventListener("click", (e) => {
     /* para evitar que se recarge la pagina */
     e.preventDefault();
-    console.log(1)
     /* para que la variable del texto sea lo que la persona escriba en el input*/
     const text= input.value
-    const li= document.createElement('li');
-    const p= document.createElement('p');
-    p.textContent= text;
-    li.appendChild(p);
-    ul.appendChild(li);
+    if (text !== "") {
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.textContent = text;
     
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+    
+        input.value = "";
+        empty.style.display = "none";
+      }
   });
