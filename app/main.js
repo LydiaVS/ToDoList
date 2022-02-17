649bdcc8943b4104dec71f7517b51ab6cf38f12b
LydiaVS <lidia.vazquez98@gmail.com>
2022-02-17 16:08:39 +0100

    final

--- app/main.js
+++ app/main.js
@@ -1,24 +1,16 @@
-window.onload = () => {
-    //Añadir botón eliminar
-    function addDeleteBtn() {
-      const deleteBtn = document.createElement("button");
-    
-      deleteBtn.textContent = "X";
-      deleteBtn.className = "btn-delete";
-    //Evento eliminar
-      deleteBtn.addEventListener("click", (e) => {
-        const item = e.target.parentElement;
-        ul.removeChild(item);
-    
-        const items = document.querySelectorAll("li");
-    
-        if (items.length === 0) {
-          empty.style.display = "block";
-        }
-      });
-    
-      return deleteBtn;
-    }
+const input = document.querySelector("input");
+const add = document.querySelector(".add");
+const ul = document.querySelector("ul");
 
-
-}
+add.addEventListener("click", (e) => {
+    /* para evitar que se recarge la pagina */
+    e.preventDefault();
+    console.log(1)
+    /* para que la variable del texto sea lo que la persona escriba en el input*/
+    const text= input.value
+    const li= document.createElement('li');
+    const p= document.createElement('p');
+    p.textContent= text;
+    li.appendChild(p);
+    ul.appendChild(li);   
+  });
