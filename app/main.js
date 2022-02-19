97cf51a7ab8fcc0435a94b0fb2dcda7e35d4028f
LydiaVS <lidia.vazquez98@gmail.com>
2022-02-19 14:00:08 +0100

    final-final

--- app/main.js
+++ app/main.js
@@ -1,8 +1,9 @@
-const input = document.querySelector("input");
-const add = document.querySelector(".add");
-const ul = document.querySelector("ul");
+window.onload = () => {
+    const input = document.querySelector("input");
+    const add = document.querySelector(".add");
+    const ul = document.querySelector("ul");
 
-add.addEventListener("click", (e) => {
+    add.addEventListener("click", (e) => {
     /* para evitar que se recarge la pagina */
     e.preventDefault();
     console.log(1)
@@ -12,5 +13,29 @@ add.addEventListener("click", (e) => {
     const p= document.createElement('p');
     p.textContent= text;
     li.appendChild(p);
+    li.appendChild(addDeleteBtn());
     ul.appendChild(li);   
   });
+    //Añadir botón eliminar
+    function addDeleteBtn() {
+      const deleteBtn = document.createElement("button");
+    
+      deleteBtn.textContent = "X";
+      deleteBtn.className = "btn-delete";
+    //Evento eliminar
+      deleteBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+    
+        const items = document.querySelectorAll("li");
+    
+        if (items.length === 0) {
+          empty.style.display = "block";
+        }
+      });
+    
+      return deleteBtn;
+    }
+
+
+}
